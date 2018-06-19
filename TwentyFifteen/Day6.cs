9db9085b3324300233cab40528edbcd73b709b70
Kyle Baker <kbaker1@kabbage.com>
2018-06-19 15:28:46 -0400

    Day6 descriptions

--- TwentyFifteen/Day6.cs
+++ TwentyFifteen/Day6.cs
@@ -69,7 +69,28 @@ namespace AdventOfCode.TwentyFifteen
             return (x: split[0], y: split[1]);
         }
 
-        // Solves Part1
+        // Lights in your grid are numbered from 0 to 999 in each direction;
+        // the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The
+        // instructions include whether to turn on, turn off, or toggle various
+        // inclusive ranges given as coordinate pairs. Each coordinate pair
+        // represents opposite corners of a rectangle, inclusive; a coordinate
+        // pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3
+        // square. The lights all start turned off.
+        //
+        // To defeat your neighbors this year, all you have to do is set up
+        // your lights by doing the instructions Santa sent you in order.
+        //
+        // For example:
+        //
+        //   - turn on 0,0 through 999,999 would turn on (or leave on) every
+        //     light.
+        //   - toggle 0,0 through 999,0 would toggle the first line of 1000
+        //     lights, turning off the ones that were on, and turning on the
+        //     ones that were off.
+        //   - turn off 499,499 through 500,500 would turn off (or leave off)
+        //     the middle four lights.
+        //
+        // After following the instructions, how many lights are lit?
         private static int Part1(IEnumerable<(Switch, (int x, int y), (int x, int y))> input)
         {
             var finalGrid = input.Aggregate(createEmptyGrid(), (grid, instruction) => {
@@ -98,7 +119,28 @@ namespace AdventOfCode.TwentyFifteen
             return finalGrid.Where(kv => kv.Value == 1).Count();
         }
 
-        // Solves Part2
+        // The light grid you bought actually has individual brightness
+        // controls; each light can have a brightness of zero or more. The
+        // lights all start at zero.
+        //
+        // The phrase turn on actually means that you should increase the
+        // brightness of those lights by 1.
+        //
+        // The phrase turn off actually means that you should decrease the
+        // brightness of those lights by 1, to a minimum of zero.
+        //
+        // The phrase toggle actually means that you should increase the
+        // brightness of those lights by 2.
+        //
+        // What is the total brightness of all lights combined after following
+        // Santa's instructions?
+        //
+        // For example:
+        //
+        //   - turn on 0,0 through 0,0 would increase the total brightness by
+        //     1.
+        //   - toggle 0,0 through 999,999 would increase the total brightness
+        //     by 2000000.
         private static int Part2(IEnumerable<(Switch, (int x, int y), (int x, int y))> input)
         {
             return 42;
